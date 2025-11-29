bf0f91e736be0f2731f8ef7171a076183fa0e984
Jatin <jatinpathania1@gmail.com>
2025-11-29 14:24:43 +0530

    admin file process exit applied while changing database to monogodb atlas and inserting admin to it firstly by running script

--- TaskNet/Backend/public/admin.js
+++ TaskNet/Backend/public/admin.js
@@ -12,11 +12,11 @@ const createAdmin= async()=>{
         const existing= await User.findOne({email})
         if(existing){
             console.log("Admin alredy exists")
-            process.exit(1);
+            process.exit(0);
         }
-        const hashedPassword= await bcrypt.hash(password,10);
-        await User.create({name , email, password: hashedPassword, role})
+        await User.create({name , email, password: password, role})
         console.log("Admin created successfully")
+        process.exit(0);
     }
     catch(e){
         console.log("eror creating admin", e)
